981299e837dc6aee0fd6cd281eec020ef40ec8ba
begalvan <begalvan@miners.utep.edu>
2019-09-08 18:26:06 -0600

    analysis, comment, output boxes

--- CommentView.py
+++ CommentView.py
@@ -13,29 +13,34 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(286, 215)
-        self.pushButtonSave = QtWidgets.QPushButton(Dialog)
-        self.pushButtonSave.setGeometry(QtCore.QRect(90, 180, 93, 28))
-        self.pushButtonSave.setObjectName("pushButtonSave")
-        self.pushButtonClear = QtWidgets.QPushButton(Dialog)
-        self.pushButtonClear.setGeometry(QtCore.QRect(190, 180, 93, 28))
-        self.pushButtonClear.setObjectName("pushButtonClear")
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(100, 10, 91, 16))
-        self.label.setObjectName("label")
+        Dialog.resize(689, 433)
+        self.gridLayout = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout.setObjectName("gridLayout")
         self.plainTextEdit_CommView = QtWidgets.QPlainTextEdit(Dialog)
-        self.plainTextEdit_CommView.setGeometry(QtCore.QRect(10, 40, 271, 131))
         self.plainTextEdit_CommView.setObjectName("plainTextEdit_CommView")
+        self.gridLayout.addWidget(self.plainTextEdit_CommView, 1, 1, 1, 3)
+        self.pushButtonSave = QtWidgets.QPushButton(Dialog)
+        self.pushButtonSave.setMaximumSize(QtCore.QSize(100, 16777215))
+        self.pushButtonSave.setObjectName("pushButtonSave")
+        self.gridLayout.addWidget(self.pushButtonSave, 2, 2, 1, 1)
+        self.pushButtonClear = QtWidgets.QPushButton(Dialog)
+        self.pushButtonClear.setMaximumSize(QtCore.QSize(100, 16777215))
+        self.pushButtonClear.setObjectName("pushButtonClear")
+        self.gridLayout.addWidget(self.pushButtonClear, 2, 3, 1, 1)
+        self.frame = QtWidgets.QFrame(Dialog)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.gridLayout.addWidget(self.frame, 0, 1, 3, 3)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        Dialog.setWindowTitle(_translate("Dialog", "Comment View"))
         self.pushButtonSave.setText(_translate("Dialog", "Save"))
         self.pushButtonClear.setText(_translate("Dialog", "Clear"))
-        self.label.setText(_translate("Dialog", "Comment View"))
 
 
 if __name__ == "__main__":
