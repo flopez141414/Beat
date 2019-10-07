bfa7698fa0c35dc89e11369c2f64ce4214bfadd1
flopez141414 <flopez141414@gmail.com>
2019-10-06 21:19:58 -0600

    "merged doc and project tabs"

--- functions/tabs/beatmain.py
+++ functions/tabs/beatmain.py
@@ -2,18 +2,20 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QTabWidget
 
 from PointsOfInterestTab import PointsOfInterestTab
+from DocumentationTab import DocumentationTab
+from projectTab import ProjectTab
 
 def main():
-    #initialinze stuff
+    #initialize stuff
     app = QApplication([])
     mainWindow = QMainWindow()
 
     tabWidget = QTabWidget()
-    #tabWidget.addTab(ProjectTab(), "project tab")
+    tabWidget.addTab(ProjectTab(), "project tab")
    # tabWidget.addTab(AnalysisTab(), "AnalysisTab")
     #tabWidget.addTab(PluginManagementTab(), "PluginManagementTab")
     tabWidget.addTab(PointsOfInterestTab(), "PointsOfInterestTab")
-   # tabWidget.addTab(DocumentationTab(), "DocumentationTab")
+    tabWidget.addTab(DocumentationTab(), "DocumentationTab")
 
     mainWindow.setWindowTitle("BEAT: Behavior Extraction and Analysis Tool")
     mainWindow.setCentralWidget(tabWidget)
