959da00c35e3cbaa8a373d089f1a85fc585aea55
flopez141414 <flopez141414@gmail.com>
2019-10-06 23:07:19 -0600

    "pushed changes to dev"

--- explorerDdemo.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
-
-root = Tk()
-ftypes = [('htm file',"*.htm")]
-ttl  = "Title"
-dir1 = 'C:\\'
-root.fileName = askopenfilename(filetypes = ftypes, initialdir = dir1, title = ttl)
-print (root.fileName)
--- tempController.py
+++
@@ -1,2 +0,0 @@
-from functions import CRUD
-import mainLayout
