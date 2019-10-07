34f3d4017dbd33a454922f4784887884b16d79cc
flopez141414 <flopez141414@gmail.com>
2019-10-06 21:58:04 -0600

    "merged analysis tab"

--- functions/tabs/beatmain.py
+++ functions/tabs/beatmain.py
@@ -1,9 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QTabWidget
 
+import r2pipe
+import pymongo
 from PointsOfInterestTab import PointsOfInterestTab
 from DocumentationTab import DocumentationTab
 from projectTab import ProjectTab
+from analysisTab import AnalysisTab
 
 def main():
     #initialize stuff
@@ -12,7 +15,7 @@ def main():
 
     tabWidget = QTabWidget()
     tabWidget.addTab(ProjectTab(), "project tab")
-   # tabWidget.addTab(AnalysisTab(), "AnalysisTab")
+    tabWidget.addTab(AnalysisTab(), "AnalysisTab")
     #tabWidget.addTab(PluginManagementTab(), "PluginManagementTab")
     tabWidget.addTab(PointsOfInterestTab(), "PointsOfInterestTab")
     tabWidget.addTab(DocumentationTab(), "DocumentationTab")
