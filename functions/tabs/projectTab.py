476c4f37075ab9d1a36b5c01ad065d2b599b8369
Rocio Cardona <rcardona4@miners.utep.edu>
2019-10-14 19:54:41 -0600

    able to extract properties from binary file

--- functions/tabs/projectTab.py
+++ functions/tabs/projectTab.py
@@ -1,5 +1,10 @@
+import json
+
+import r2pipe
+from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QTableWidgetItem, QTableWidget, QWidget, QPushButton, QLabel, QGridLayout, QTextEdit, QLineEdit, QListWidget
+from PyQt5.QtWidgets import QTableWidgetItem, QTableWidget, QWidget, QPushButton, QLabel, QGridLayout, QTextEdit, \
+    QLineEdit, QListWidget, QFileDialog
 
 
 class ProjectTab(QWidget):
@@ -10,7 +15,7 @@ class ProjectTab(QWidget):
         leftLayout = QGridLayout()
         rightLayout = QGridLayout()
         mainlayout.addLayout(leftLayout, 1, 0, 6, 1)
-        mainlayout.addLayout(rightLayout, 1, 1, 6, 1)
+        mainlayout.addLayout(rightLayout, 1, 1, 6, 5)
 
         # Left panel
         searchBox = QLineEdit()
@@ -20,10 +25,11 @@ class ProjectTab(QWidget):
         leftPanelLabel = QLabel('Project View')
         leftPanelLabel.setAlignment(Qt.AlignCenter)
 
-        leftLayout.addWidget(leftPanelLabel, 0, 0, 1, 5)
+        leftLayout.addWidget(leftPanelLabel, 0, 0, 1, 4)
         leftLayout.addWidget(searchBox, 1, 0, 1, 3)
-        leftLayout.addWidget(searchButton, 1, 4, 1, 1)
-        leftLayout.addWidget(searchList, 2, 0, 1, 5)
+        leftLayout.addWidget(searchButton, 1, 3, 1, 1)
+        leftLayout.addWidget(searchList, 2, 0, 1, 4)
+
         leftLayout.addWidget(newButton, 6, 0)
 
         # Right panel
@@ -31,60 +37,86 @@ class ProjectTab(QWidget):
         rightPanelLabel.setAlignment(Qt.AlignCenter)
         projNameArea = QTextEdit()
         projDescriptionArea = QTextEdit()
-        binaryFilePath = QTextEdit()
-        binaryFileProp = QTextEdit()
+        self.binaryFilePath = QTextEdit()
+        self.binaryFileProp = QTableWidget()
+        self.binaryFileProp.horizontalHeader().setStretchLastSection(True)
+        self.binaryFileProp.verticalHeader().setVisible(False)
+        self.binaryFileProp.horizontalHeader().setVisible(False)
+        self.binaryFileProp.setAlternatingRowColors(True)
         browseButton = QPushButton('Browse')
+        browseButton.clicked.connect(self.OpenFile)
 
-
-        rightLayout.addWidget(rightPanelLabel, 0, 0, 1, 10)
-        rightLayout.addWidget(projNameArea, 1, 2, 10, 2)
-        rightLayout.addWidget(projDescriptionArea, 2, 2, 10, 2)
-        rightLayout.addWidget(binaryFilePath, 4, 2, 10, 2)
-        rightLayout.addWidget(binaryFileProp, 5, 2, 10, 2)
-        rightLayout.addWidget(browseButton, 3, 6)
+        rightLayout.addWidget(rightPanelLabel, 0, 0, 1, 14)
+        rightLayout.addWidget(projNameArea, 1, 2, 10, 10)
+        rightLayout.addWidget(projDescriptionArea, 2, 2, 5, 10)
+        rightLayout.addWidget(self.binaryFilePath, 4, 2, 10, 10)
+        rightLayout.addWidget(self.binaryFileProp, 6, 2, 8, 10)
+        rightLayout.addWidget(browseButton, 4, 12)
         rightLayout.addWidget(QLabel('Project Name'), 1, 1, 1, 1)
         rightLayout.addWidget(QLabel('Project Description'), 2, 1, 1, 1)
-        rightLayout.addWidget(QLabel('Binary File Path'), 4, 1, 1, 1)
+        rightLayout.addWidget(QLabel('Binary File Path'), 6, 1, 1, 1)
         rightLayout.addWidget(QLabel('Binary File Properties'), 5, 1, 1, 1)
 
         deleteButton = QPushButton('Delete')
         saveButton = QPushButton('Save')
-        rightLayout.addWidget(saveButton, 15, 7)
+        rightLayout.addWidget(saveButton, 15, 8)
         rightLayout.addWidget(deleteButton, 15, 1)
 
         button = QPushButton("My Button")
-        button.clicked.connect(self.clickEvent)
+        deleteButton.clicked.connect(self.staticAnalysis)
         self.setLayout(mainlayout)
-        
-        self.infoTable = QTableWidget()
-        self.infoTable.setRowCount(2)
-        self.infoTable.setColumnCount(2)
-        self.infoTable.setItem(0, 0, QTableWidgetItem("OS"))
-        self.infoTable.setItem(1, 0, QTableWidgetItem("Binary Type"))
-        self.infoTable.doubleClicked.connect(self.on_click)
-#         rightLayout.addWidget(self.infoTable)
+
+        self.binaryFileProp.setRowCount(12)
+        self.binaryFileProp.setColumnCount(2)
+        self.binaryFileProp.setItem(0, 0, QTableWidgetItem("OS"))
+        self.binaryFileProp.setItem(1, 0, QTableWidgetItem("Binary Type"))
+        self.binaryFileProp.setItem(2, 0, QTableWidgetItem("Machine"))
+        self.binaryFileProp.setItem(3, 0, QTableWidgetItem("Class"))
+        self.binaryFileProp.setItem(4, 0, QTableWidgetItem("Bits"))
+        self.binaryFileProp.setItem(5, 0, QTableWidgetItem("Canary"))
+        self.binaryFileProp.setItem(6, 0, QTableWidgetItem("Crypto"))
+        self.binaryFileProp.setItem(7, 0, QTableWidgetItem("Nx"))
+        self.binaryFileProp.setItem(8, 0, QTableWidgetItem("Pic"))
+        self.binaryFileProp.setItem(9, 0, QTableWidgetItem("Relocs"))
+        self.binaryFileProp.setItem(10, 0, QTableWidgetItem("Relro"))
+        self.binaryFileProp.setItem(11, 0, QTableWidgetItem("Stripped"))
+        self.binaryFileProp.doubleClicked.connect(self.on_click)
 
     def clickEvent(self):
         print("Clicked")
-        
+
     def on_click(self):
         print("\n")
         for currentQTableWidgetItem in self.infoTable.selectedItems():
             print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
-            
-    
 
-    def staticAnalysis(self):
-        rlocal = r2pipe.open("/bin/ping")
-        binInfo = rlocal.cmd('ij')
-        data = json.loads(binInfo)
-#         myvalue = (data['core']['file'])
-#         print(myvalue)
-  
-        myvalue = (data['core']['file'])
-        for key in data:
-            print(data['core']['file'])
-            
-        self.infoTable.setItem(1,1,QTableWidgetItem(myvalue))
+    def staticAnalysis(self, filename):
+        binPropertiesList = ["os", "bintype", "machine", "class", "bits", "canary", "crypto", "nx", "pic", "relocs",
+                             "relro", "stripped"]
 
-#     def populateBinaryInfo(self):
+        # filename = self.OpenFile()
+        rlocal = r2pipe.open(filename)
+        # list1 = rlocal.cmd('iI')
+        binInfo = rlocal.cmd('iI').splitlines()
+        # print(binInfo)
+
+        colNum = 0
+        for item in binPropertiesList:
+            matchingline = [s for s in binInfo if item in s]
+            print(matchingline)
+            a = matchingline[0].split()
+            self.binaryFileProp.setItem(colNum, 1, QTableWidgetItem(a[1]))
+            colNum += 1
+
+    def OpenFile(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open file", "",
+                                                  "All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
+            self.binaryFilePath.setText(fileName)
+            self.staticAnalysis(fileName)
+            return fileName
+
+        return "not found"
