2534b005ae128c55e36740fa5b3f9399cf9acf58
Brian Cardiel <42951440+bacardiel05@users.noreply.github.com>
2019-10-26 19:19:58 -0600

    + Added DB upload + Added XML methods + XML can be saved to DB

---
+++ functions/tabs/xmlTest.py
@@ -0,0 +1,15 @@
+import xml.etree.ElementTree as ET
+tree = ET.parse('practiceXml.xml')
+root = tree.getroot()
+
+#iterate on xml
+for x in root.iter('PointOfInterest'):
+    print(x.attrib)  # x.text
+
+#modyfying XML
+b2tf = root.find("./Project_name")
+print(b2tf)
+b2tf.text = "Back to the Future"
+print(b2tf.attrib)
+print('********************************')
+print(ET.tostring(root, encoding='utf8').decode('utf8'))
