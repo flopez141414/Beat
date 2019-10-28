9291cc9e6648a4b7fdd3d30af8fe749cca18e694
Brian Cardiel <42951440+bacardiel05@users.noreply.github.com>
2019-10-27 21:30:38 -0600

    + Started adding functions to plugin management + Preparing to merge with Rocio's code

--- functions/tabs/PluginManagementTab.py
+++ functions/tabs/PluginManagementTab.py
@@ -7,6 +7,9 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot, Qt
 from PyQt5 import QtCore, QtGui
 
+pluginNameN = ''
+pluginDescH = ''
+poiH = ''
 
 class PluginManagementTab(QWidget):
     def __init__(self):
@@ -39,25 +42,29 @@ class PluginManagementTab(QWidget):
         rightPanelLabel.setAlignment(Qt.AlignCenter)
 #         rightPanelLabel.setStyleSheet("background-color: rgba(173,216,230 ,1 )")
         rightPanelLabel.setFont(QtGui.QFont('Arial', 12, weight=QtGui.QFont().Bold))
-        pluginStructArea = QTextEdit()
-        pluginDataSet = QTextEdit()
-        pluginName = QTextEdit()
+        self.pluginStructArea = QTextEdit()
+        self.pluginDataSet = QTextEdit()
+        self.pluginName = QTextEdit()
         pluginDesc = QTextEdit()
-        pointsOI = QTextEdit()
+        self.pointsOI = QTextEdit()
         defaultOutDropdown = QComboBox()
+
         browseButton1 = QPushButton('Browse')
+        browseButton1.clicked.connect(self.OpenPluginFile)
+
         browseButton2 = QPushButton('Browse')
+        browseButton2.clicked.connect(self.OpenPluginFile)
 
         rightLayout.addWidget(browseButton1, 1, 6)
         rightLayout.addWidget(browseButton2, 2, 6)
 
         rightLayout.addWidget(rightPanelLabel, 0, 0, 1, 10)
-        rightLayout.addWidget(pluginStructArea, 1, 2, 2, 1)
-        rightLayout.addWidget(pluginDataSet, 2, 2, 2, 1)
-        rightLayout.addWidget(pluginName, 3, 2, 2, 1)
+        rightLayout.addWidget(self.pluginStructArea, 1, 2, 2, 1)
+        rightLayout.addWidget(self.pluginDataSet, 2, 2, 2, 1)
+        rightLayout.addWidget(self.pluginName, 3, 2, 2, 1)
         rightLayout.addWidget(pluginDesc, 4, 2, 2, 1)
         rightLayout.addWidget(defaultOutDropdown, 5, 2, 1, 1)
-        rightLayout.addWidget(pointsOI, 6, 2, 4, 1)
+        rightLayout.addWidget(self.pointsOI, 6, 2, 4, 1)
 
         rightLayout.addWidget(QLabel('Plugin Structure'), 1, 1, 1, 1)
         rightLayout.addWidget(QLabel('Plugin Predefined Data Set'), 2, 1, 1, 1)
@@ -77,3 +84,22 @@ class PluginManagementTab(QWidget):
 
     def clickEvent(self):
         print("Clicked")
+
+
+    def OpenPluginFile(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open file", "",
+                                                  "All Files (*);;Python Files (*.xml)", options=options)
+        if fileName:
+            print('test')
+            self.pluginStructArea.setText('Hello!')
+            self.pluginDataSet.setText('world')
+            self.pluginName.setText('asaa')
+            self.pointsOI.setText('list of pois go heres')
+            #global myFileName
+            #myFileName = fileName
+
+            #return fileName
+
+        #return "not found"
